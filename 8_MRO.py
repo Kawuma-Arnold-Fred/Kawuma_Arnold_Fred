481de51473d58f757f11330ea320ea9e137b82bf
poppenheimer <kawumaarnoldf@gmail.com>
2025-05-27 20:01:49 +0300

    MROed

---
+++ 8_MRO.py
@@ -0,0 +1,18 @@
+class Payment:
+    def pay(self):
+        print('Scanning items')
+        
+class CreditCard(Payment):
+    def pay(self):
+        print('PAying with credit card')
+        
+class DigitalPayment(Payment):
+    def pay(self):
+        print('Paying with mobile app')
+        
+class PaymentMethod(CreditCard, DigitalPayment):
+    pass
+
+Fred = DigitalPayment()
+Fred.pay()
+print(f'Method Resolution Order: {PaymentMethod.__mro__}')
